5874387a2a309a46ea29e7a29845d0136c0e7e06
xiongsch-1 <xiongsch@163.com>
2026-06-30 12:10:22 +0800

    springcloud_4

--- customer/src/main/java/com/zhaowa/galaxy/springcloud/customer/controller/CustomerController.java
+++ customer/src/main/java/com/zhaowa/galaxy/springcloud/customer/controller/CustomerController.java
@@ -4,19 +4,28 @@ package com.zhaowa.galaxy.springcloud.customer.controller;
 import com.zhaowa.galaxy.springcloud.customer.service.CustomerService;
 import com.zhaowa.galaxy.springcloud.servicea.api.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/customer")
+@RefreshScope
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    @Value("${disableRequest:false}")
+    private Boolean disable;
+
     @GetMapping("/test1")
     @ResponseBody
     public Result test1(@RequestParam String name1, @RequestParam String name2) {
+        if (disable) {
+            return new Result(0, "remote service disabled!");
+        }
         return customerService.testPost(name1,name2);
     }
 
